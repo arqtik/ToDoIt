3aa0f6c99a59042e93687ab10d290169ba6df5a6
arqtik <martynascamets1@gmail.com>
2021-10-11 11:47:14 +0200

    Fixed ToDo
    Fixed ToDo with properties and added tests

---
+++ ToDoIt.Tests/ToDoTests.cs
@@ -0,0 +1,22 @@
+﻿using ToDoIt.Model;
+using Xunit;
+
+namespace ToDoIt.Tests
+{
+    public class ToDoTests
+    {
+        [Fact]
+        private void ToDoTest()
+        {
+            Person person = new Person("Billy", "Johnson", 42);
+            ToDo toDo = new ToDo(1, "Test Desc")
+            {
+                Assignee = person,
+                Done = true
+            };
+
+            Assert.Equal(person, toDo.Assignee);
+            Assert.True(toDo.Done);
+        }
+    }
+}
--- ToDoIt/Model/ToDo.cs
+++ ToDoIt/Model/ToDo.cs
@@ -4,8 +4,16 @@
     {
         private readonly int todoId;
         private string description;
-        private bool done;
-        private Person assignee;
+
+        /// <summary>
+        /// If the To-Do is done or not
+        /// </summary>
+        public bool Done { get; set; }
+
+        /// <summary>
+        /// Assignee person of this To-Do
+        /// </summary>
+        public Person Assignee { get; set; }
 
         public ToDo(int todoId, string description)
         {
