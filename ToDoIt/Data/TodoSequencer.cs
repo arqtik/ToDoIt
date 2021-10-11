0cb9eed75f293270571ed42cb1eb669ecc01c49a
arqtik <martynascamets1@gmail.com>
2021-10-11 12:03:07 +0200

    Create TodoSequencer.cs

---
+++ ToDoIt/Data/TodoSequencer.cs
@@ -0,0 +1,25 @@
+﻿namespace ToDoIt.Data
+{
+    public class TodoSequencer
+    {
+        private static int todoId;
+
+        /// <summary>
+        /// Get the id for next to-do
+        /// </summary>
+        /// <returns>ID for the to-do</returns>
+        public static int nextTodoId()
+        {
+            todoId++;
+            return todoId;
+        }
+
+        /// <summary>
+        /// Resets the todoId counter to 0
+        /// </summary>
+        public static void reset()
+        {
+            todoId = 0;
+        }
+    }
+}
