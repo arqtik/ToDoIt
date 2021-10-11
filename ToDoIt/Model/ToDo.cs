ceecb378f46b4d101464af52f3844e4c1eef6a84
arqtik <martynascamets1@gmail.com>
2021-10-11 10:58:12 +0200

    Create ToDo.cs

---
+++ ToDoIt/Model/ToDo.cs
@@ -0,0 +1,16 @@
+﻿namespace ToDoIt.Model
+{
+    public class ToDo
+    {
+        private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+        public ToDo(int todoId, string description)
+        {
+            this.todoId = todoId;
+            this.description = description;
+        }
+    }
+}
