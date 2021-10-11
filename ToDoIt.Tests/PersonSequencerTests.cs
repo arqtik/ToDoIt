74491dc70038d8e97516841bb8630e7d3403126d
arqtik <martynascamets1@gmail.com>
2021-10-11 11:57:36 +0200

    Added PersonSequencer.cs
    Including tests

---
+++ ToDoIt.Tests/PersonSequencerTests.cs
@@ -0,0 +1,33 @@
+﻿using ToDoIt.Data;
+using Xunit;
+
+namespace ToDoIt.Tests
+{
+    public class PersonSequencerTests
+    {
+        [Fact]
+        private void nextPersonIdTest()
+        {
+            int firstId = PersonSequencer.nextPersonId();
+            int secondId = PersonSequencer.nextPersonId();
+            
+            Assert.Equal(firstId + 1, secondId);
+        }
+
+        [Fact]
+        private void resetTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                PersonSequencer.nextPersonId();
+            }
+            
+            PersonSequencer.reset();
+
+            int expected = 1;
+            int actual = PersonSequencer.nextPersonId();
+            
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ ToDoIt/Data/PersonSequencer.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Xml;
+
+namespace ToDoIt.Data
+{
+    public class PersonSequencer
+    {
+        private static int personid;
+
+        /// <summary>
+        /// Get the id for the next person
+        /// </summary>
+        /// <returns>ID for the next person</returns>
+        public static int nextPersonId()
+        {
+            personid++;
+            return personid;
+        }
+
+        /// <summary>
+        /// Reset PersonID counter to 0
+        /// </summary>
+        public static void reset()
+        {
+            personid = 0;
+        }
+    }
+}
