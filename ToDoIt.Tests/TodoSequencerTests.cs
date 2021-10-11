a2a9e10f0f19ede5f0657b5cabf3ead29ee67fb6
arqtik <martynascamets1@gmail.com>
2021-10-11 12:05:31 +0200

    Create TodoSequencerTests.cs

---
+++ ToDoIt.Tests/TodoSequencerTests.cs
@@ -0,0 +1,33 @@
+﻿using ToDoIt.Data;
+using Xunit;
+
+namespace ToDoIt.Tests
+{
+    public class TodoSequencerTests
+    {
+        [Fact]
+        private void nextTodoIdTest()
+        {
+            int firstId = TodoSequencer.nextTodoId();
+            int secondId = TodoSequencer.nextTodoId();
+            
+            Assert.Equal(firstId + 1, secondId);
+        }
+        
+        [Fact]
+        private void resetTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                TodoSequencer.nextTodoId();
+            }
+            
+            TodoSequencer.reset();
+
+            int expected = 1;
+            int actual = TodoSequencer.nextTodoId();
+            
+            Assert.Equal(expected, actual);
+        }
+    }
+}
